cc3592ae0f92e3dde0132dcd297d9f4dab461196
veroniq04 <veroniq04@users.noreply.github.com>
2019-08-14 21:34:30 -0400

    Function combining molecular reaction and effector-target interactions model

---
+++ code/adcx.m
@@ -0,0 +1,65 @@
+function [T,E,Estar,LDH,perf,CPX] = adcx(tf_mol,tf_et,nr_t_mol,nr_t_et,T0,E0,g,r,kexp,gamma,...
+    CD20,CD16,RTX,kon20,koff20,kon16,koff16,gamma_perf)
+
+close all
+A0 = CD20;
+C0 = CD16;
+R0 = RTX;
+
+% Run molecular rate equations to steady state
+f_rate = reactions(A0,R0,C0,kon20,kon16,koff20,koff16,tf_mol,nr_t_mol);
+
+%% Effector-target code
+
+% Initial condition
+y0(1) = T0;  % T target cell
+y0(2) = E0;  % E effector cell CD16
+y0(3) = 0;   % D dead cell
+NE    = y0(2); % only E, no E* at first
+
+pars = [g,r,kexp,NE,gamma,f_rate];
+
+%% Simulate the Effector-Target ODEs
+tspan = linspace(0,tf_et,nr_t_et);
+[t,y] = ode45(@et_code_rhs,tspan,y0,[],pars);
+
+% Store variables
+T = y(:,1);
+E = y(:,2);
+Estar = NE-E;
+LDH   = y(:,3);
+perf  = gamma_perf.*y(:,3);
+CPX   = f_rate;
+
+% %% Plotting
+% figure(1)
+% plot(t,y(:,1),'LineWidth',2)
+% hold on
+% plot(t,y(:,2),'LineWidth',2)
+% plot(t,y(:,3),'LineWidth',2)
+% ylim([0 5.5])
+% legend('Target cells','Effector cells','Dead cells/lysis')
+% xlabel('Time')
+% ylabel('Cells/volume')
+% set(gca,'FontSize',16)
+
+
+% % Parameters f(R,CD16,CD20) function
+% kon20  = 1;
+% kon16  = 0.5;
+% koff20 = 0.2;
+% koff16 = 0.1;
+% tf_mol = 200;
+% nr_t_mol = 1000;
+
+% % Parameters et_code
+% g    = 0.01;
+% r    = 5;
+% kexp = 1;
+% gamma = 0.5;
+%
+% T0 = 2;  % T target cell
+% E0 = 5;  % E effector cell CD16
+% CD20 = 20;
+% CD16 = 30;
+% RTX = 10^1;
--- code/et_code_rhs.m
+++ code/et_code_rhs.m
@@ -9,29 +9,17 @@ function f = et_code_rhs(t,y,pars)
     r     = pars(2);
     kexp  = pars(3);
     NE    = pars(4);
-    
-    pars_f = pars(5:end);
+    gamma = pars(5);
+    f_rate= pars(6);
    
-    f(1)  = g*T - fcn_f(pars_f)*E^r*T;
-    f(2)  = kexp*(NE-E) - r*fcn_f(pars_f)*E^r*T;
-    f(3)  = fcn_f(pars_f)*E^r*T;
+    f(1)  = g*T - fcn_f(f_rate,gamma)*E^r*T;
+    f(2)  = kexp*(NE-E) - r*fcn_f(f_rate,gamma)*E^r*T;
+    f(3)  = fcn_f(f_rate,gamma)*E^r*T;
 
 end
 
-function f = f_molecular(pars_f)
+function g = fcn_f(f_rate,gamma)
     
-    kdRCD20 = pars_f(1);
-    kdRCD16 = pars_f(2);
-    CD20    = pars_f(4);
-    CD16    = pars_f(5);
-    R       = pars_f(6);
-    f = R.*(CD20/(kdRCD20+CD20) + CD16/(kdRCD16+CD16));
-
-end
-
-function g = fcn_f(pars_f)
-    
-    gamma = pars_f(3);
-    g = gamma*f_molecular(pars_f);
+    g     = gamma*f_rate;
 
 end
