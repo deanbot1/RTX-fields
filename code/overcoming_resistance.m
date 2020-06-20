6354f90919718aa0dc6d70dc4507759eddf1a7f9
Dean <dean.bottino@takeda.com>
2020-06-20 18:43:12 -0400

    created new out folder for raw figure outputs. MOved last run pngs to there Got overcoming_resistance.m gneralized and used it to make CD20/CD16 and CD20/gamma plots.

--- code/overcoming_resistance.m
+++ code/overcoming_resistance.m
@@ -1,45 +1,62 @@
 %% how much CD16 upregulation is needed to overcome a given degree of CD20 loss?
 clear all; close all
 
-par = readtable('parameter_estimation_results.csv');
-par.Properties.RowNames = par.paramnames;
+Xname = 'CD20'; % specify X parameter name for the sensitivity analysis
+Yname = 'gamma'; % Y parameter name
+RTXconc = 100; % Rituximab concentration
+
+par = readtable('adcx_parameter_results_fat.csv');
+par.Properties.RowNames = par.name;
 for j = 1:height(par);
-	pdef.(par.target{j}) = par.bestest(j);
+	pdef_V158.(par.name{j}) = par.E2_V158onSUDHL4(j);
+	pdef_F158.(par.name{j}) = par.E4_F158onSUDHL4(j);
 end
 
 %v2s = @(vec)vect2struct(vec,par.Properties.RowNames); % local function converting parameter vector to parameter structure
 
-CD16def = pdef.CD16;
-CD20def = pdef.CD20;
 
-CD16range = CD16def*(10.^[-1:.1:3]);
-%CD20range = CD20def*(10.^[-10:.25:0]);
-CD20range = CD20def*(10.^[-3:.1:1]);
+for kk = 1:2
+	switch kk
+		case 1
+			pdef=pdef_V158;
+			titl='V158';
+		case 2
+			pdef=pdef_F158;
+			titl='F158';
+	end
 
-[XX,YY] = meshgrid(CD20range,CD16range);
+Ydef = pdef.(Yname);
+Xdef = pdef.(Xname);
+
+Yrange = Ydef*(10.^[-1:.1:2]);
+%Xrange = Xdef*(10.^[-10:.25:0]);
+Xrange = Xdef*(10.^[-2:.1:1]);
+
+[XX,YY] = meshgrid(Xrange,Yrange);
 
 
-for i = 1:length(CD16range)
-	for j = 1:length(CD20range)
+for i = 1:length(Yrange)
+	for j = 1:length(Xrange)
 		p = pdef;
-		p.CD20 = XX(i,j);
-		p.CD16 = YY(i,j);
-		ZZ(i,j) = adcx_wrapper(p,p.RTX);
+		p.(Xname) = XX(i,j);
+		p.(Yname) = YY(i,j);
+		ZZ(i,j) = adcx_wrapper(p,RTXconc);
 	end
 end
 
-%% plot the results
+% plot the results
 figure;
 [C,h]=contourf(log10(XX),log10(YY),ZZ);
 clabel(C,h);
 colorbar;
 colormap copper
-xlabel('log_{10}CD20');
-ylabel('log_{10}CD16');
-title('%ADCC');
+xlabel(['log_{10}' Xname]);
+ylabel(['log_{10}' Yname]);
+title(['%ADCC (' titl ')']);
 caxis([0 100]);
 grid on;
 hold on
-plot(log10(CD20def*[1 1]),log10([min(CD16range) max(CD16range)]),'w-'); hold on
-plot(log10([min(CD20range) max(CD20range)]),log10(CD16def*[1 1]),'w-');
-
+plot(log10(Xdef*[1 1]),log10([min(Yrange) max(Yrange)]),'w-'); hold on
+plot(log10([min(Xrange) max(Xrange)]),log10(Ydef*[1 1]),'w-');
+print(sprintf('../out/%s_vs_%s_%s.png',Xname,Yname,titl),'-dpng');
+end
