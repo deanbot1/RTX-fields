e75f83c2949a914de842e42cf49d5646e1649c7a
Dean <dean.bottino@takeda.com>
2020-11-08 19:59:14 -0500

    minor formatting

--- code/plot_expt2.m
+++ code/plot_expt2.m
@@ -5,13 +5,13 @@ ttnames = fieldnames(expt(1).pmap); % names of target paramter names
 for j = 1:Ne
 	subplot(1,Ne,j);
 	plot(expt(j).xval,expt(j).obs,'ro','markerfacecolor','r'); hold on;
-		for k = 1:length(ttnames)
+	for k = 1:length(ttnames)
 		tname = ttnames{k};
 		pfunc = expt(j).pmap.(tname);
 		pstruct.(tname) = pfunc(pbest);
 	end
 	Ypred = expt(j).model(pstruct,xspan);
-	plot(xspan,Ypred,'b.-'); 
+	plot(xspan,Ypred,'b.-');
 	set(gca,varargin{:});
 	title(expt(j).name,'Interpreter','none');
 	xlabel(expt(j).Xname);
